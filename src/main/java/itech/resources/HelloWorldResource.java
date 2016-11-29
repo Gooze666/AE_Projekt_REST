afafced82d62014a264e5ffef178b14121826b67
ohausherr <ohausherr@web.de>
2016-11-29 13:47:46 +0100

    Revert "Add files via upload"
    This reverts commit a6add9b29a0cdb48bd17c26e0ac1ee4548fc39cb.

--- src/main/java/itech/resources/HelloWorldResource.java
+++ src/main/java/itech/resources/HelloWorldResource.java
@@ -38,27 +38,7 @@ public class HelloWorldResource extends JerseyClient {
     	BufferedReader r;
     	StringBuilder sb = new StringBuilder();
 		try {
-			r = new BufferedReader(new FileReader("/AE_Projekt_REST/src/main/resources/anmeldeformular.html"));
-		      
-			String line = null;
-			while((line=r.readLine())!=null)
-			   sb.append(line);
-			r.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-        String anmeldeFormular = sb.toString();
-        return anmeldeFormular;
-    }
-    
-    @Path("/loginscreen")
-    @GET
-    @Produces(MediaType.TEXT_HTML)
-    public String loginScreenString() {
-    	BufferedReader r;
-    	StringBuilder sb = new StringBuilder();
-		try {
-			r = new BufferedReader(new FileReader("/AE_Projekt_REST/src/main/resources/loginscreen.html"));
+			r = new BufferedReader(new FileReader( "H:\\Workspace\\AE_Projekt_REST\\src\\main\\resources\\anmeldeformular.html"));
 		      
 			String line = null;
 			while((line=r.readLine())!=null)
@@ -68,71 +48,19 @@ public class HelloWorldResource extends JerseyClient {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        String loginScreen = sb.toString();
-        return loginScreen;
+        String anmeldeFormular = sb.toString();
+        return anmeldeFormular;
     }
     
-    @Path("/anmeldung")
+    @Path("/anmelden")
     @POST
     @Produces(MediaType.TEXT_PLAIN)
     public String anmelden(
-    		@FormParam("username")String username,
-    		@FormParam("passwort")String password
+    		@FormParam("id")String id,
+    		@FormParam("vorname")String vorname
     		) {
     	
-    	return username;
-    }
-    
-    @Path("/anmeldeformularVerarbeitung")
-    @POST
-    @Produces(MediaType.TEXT_PLAIN)
-    
-    public String anmeldeformularVerarbeitung(
-    	@FormParam("firma")String firma,
-    	@FormParam("strasse")String strasse,
-    	@FormParam("hausnr")String hausnr,
-    	@FormParam("plz")String plz,
-    	@FormParam("ort")String ort,
-    	@FormParam("telefon")String telefon,
-    	@FormParam("telefax")String telefax,
-    	@FormParam("vorname")String vorname,
-    	@FormParam("name")String name,
-    	@FormParam("email")String email,
-    	@FormParam("nachname_azubi")String nachname_azubi,
-    	@FormParam("vorname_azubi")String vorname_azubi,
-    	@FormParam("geburtsdatum_azubi")String geburtsdatum_azubi,
-    	@FormParam("geburtsort_azubi")String geburtsort_azubi,
-    	@FormParam("staatsangehoerigkeit_azubi")String staatsangehoerigkeit_azubi,
-    	@FormParam("geburtsland_azubi")String geburtsland_azubi,
-    	@FormParam("strasse_azubi")String strasse_azubi,
-    	@FormParam("hausnr_azubi")String hausnr_azubi,
-    	@FormParam("plz_azubi")String plz_azubi,
-    	@FormParam("ort_azubi")String ort_azubi,
-    	@FormParam("bundesland_azubi")String bundesland_azubi,
-    	@FormParam("telefon_azubi")String telefon_azubi,
-    	@FormParam("mobil_azubi")String mobil_azubi,
-    	@FormParam("email_azubi")String email_azubi,
-    	@FormParam("nachname_erziehungsberechtigte")String nachname_erziehungsberechtigte,
-    	@FormParam("vorname_erziehungsberechtigte")String vorname_erziehungsberechtigte,
-    	@FormParam("strasse_erziehungsberechtigte")String strasse_erziehungsberechtigte,
-    	@FormParam("hausnr_erziehungsberechtigte")String hausnr_erziehungsberechtigte,
-    	@FormParam("plz_erziehungsberechtigte")String plz_erziehungsberechtigte,
-    	@FormParam("ort_erziehungsberechtigte")String ort_erziehungsberechtigte,
-    	@FormParam("telefon_erziehungsberechtigte")String telefon_erziehungsberechtigte,
-    	@FormParam("email_erziehungsberechtigte")String email_erziehungsberechtigte,
-    	@FormParam("schulname_bildungsweg")String schulname_bildungsweg,
-    	@FormParam("bundesland_bildungsweg")String bundesland_bildungsweg,
-    	@FormParam("entlassungsjahr_bildungsweg")String entlassungsjahr_bildungsweg,
-    	@FormParam("hoechsterabschluss_bildungsweg")String hoechsterabschluss_bildungsweg,
-    	@FormParam("schulbesuchsjahr_bildungsweg")String schulbesuchsjahr_bildungsweg,
-    	@FormParam("fachrichtung_ausbildung")String fachrichtung_ausbildung,
-    	@FormParam("ausbildungszeit_vom_ausbildung")String ausbildungszeit_vom_ausbildung,
-    	@FormParam("ausbildungszeit_bis_ausbildung")String ausbildungszeit_bis_ausbildung,
-    	@FormParam("begruendung_ausbildung")String begruendung_ausbildung
-    		) {
-    	
-
-        return firma;
+        return "Anmeldung als "+vorname+" mit "+id+" fehlgeschlagen";
     }
     
     @Path("/sekretaerformular")
